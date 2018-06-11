5d1abcc9da3d8ae468ee2101f2caebeadd1b35aa
Shang-Hung, Shih <adgh456@gmail.com>
2018-06-12 00:05:09 +0800

    Update NGStools.py

--- NGStools.py
+++ NGStools.py
@@ -324,3 +324,48 @@ def CNV(self):
     
 def Phial(refPath, subproject, project):
     phial(refPath, 'perl phial_analysis.pl '+subproject+' '+project)
+
+def ParaSNP(storePath, subproject, project):
+    num = project
+    in_name = os.path.join(storePath, num+'.mutect2.filter.vcf')
+    out_name = os.path.join(storePath, num+'.paraSNP.vcf')
+    out = open(out_name, 'w')
+    with open(in_name, 'r') as f:
+        for l in f.readlines():
+            if l.count('#') == 0:
+                if l.split(sep='\t')[0].split(sep='chr')[1].isnumeric() == True or l.split(sep='\t')[0].split(sep='chr')[1].endswith('X') == True or l.split(sep='\t')[0].split(sep='chr')[1].endswith('Y') == True:
+                    #"""SNV"""
+                    if len(l.split(sep='\t')[3]) == 1 and len(l.split(sep='\t')[4]) == 1:
+                        #print(l.split(sep='\t')[0].split(sep='chr')[1], l.split(sep='\t')[1], l.split(sep='\t')[1], l.split(sep='\t')[3], l.split(sep='\t')[4], 'OSCC-'+num, sep='\t')
+                        w = l.split(sep='\t')[0].split(sep='chr')[1]+'\t'+l.split(sep='\t')[1]+'\t'+l.split(sep='\t')[1]+'\t'+l.split(sep='\t')[3]+'\t'+l.split(sep='\t')[4]+'\t'+'OSCC-'+num+'\n'
+                        out.writelines(w)
+                    #"""deletion"""
+                    if len(l.split(sep='\t')[3]) != 1 and len(l.split(sep='\t')[4]) == 1:
+                        #print(l.split(sep='\t')[0].split(sep='chr')[1], int(l.split(sep='\t')[1])+1, int(l.split(sep='\t')[1])+len(l.split(sep='\t')[3][1:]), l.split(sep='\t')[3][1:], '-', 'OSCC-'+num, sep='\t')
+                        w = l.split(sep='\t')[0].split(sep='chr')[1]+'\t'+str(int(l.split(sep='\t')[1])+1)+'\t'+str(int(l.split(sep='\t')[1])+len(l.split(sep='\t')[3][1:]))+'\t'+l.split(sep='\t')[3][1:]+'\t'+'-'+'\t'+'OSCC-'+num+'\n'
+                        out.writelines(w)
+                    #"""insertion"""
+                    if len(l.split(sep='\t')[3]) == 1 and len(l.split(sep='\t')[4]) != 1:
+                        #print(l.split(sep='\t')[0].split(sep='chr')[1], int(l.split(sep='\t')[1]), int(l.split(sep='\t')[1]), '-', l.split(sep='\t')[4][1:], 'OSCC-'+num, sep='\t')
+                        w = l.split(sep='\t')[0].split(sep='chr')[1]+'\t'+str(int(l.split(sep='\t')[1]))+'\t'+str(int(l.split(sep='\t')[1]))+'\t'+'-'+'\t'+l.split(sep='\t')[4][1:]+'\t'+'OSCC-'+num+'\n'
+                        out.writelines(w)
+    out.close()
+
+    os.system('sudo docker run --rm -v {}:/data adgh456/parasnp perl annovar/table_annovar.pl data/{}.paraSNP.vcf annovar/humandb/ -buildver hg19 -out data/{}.paraSNP -remove -protocol refGene,ljb26_all -operation g,f -nastring NA -otherinfo'.format(storePath, num, num))
+    os.system('mv {}/{}.paraSNP.hg19_multianno.txt {}/{}.hg19_multianno.txt'.format(storePath, num, storePath, num))
+
+    os.system('sudo docker run -itd --rm -v {}:/data adgh456/parasnp'.format(storePath))
+    os.system('sudo docker ps -l > ParaSNPid.txt')
+    with open('ParaSNPid.txt', 'r') as f:
+        for i in f.readlines():
+            if i.startswith('CONTAINER') is False:
+                mainID = i.split('        ')[0]
+                mainBox = "sudo docker exec "+mainID+' '
+    os.system('rm ParaSNPid.txt')
+
+    os.system(mainBox+'cp /data/{}.hg19_multianno.txt .'.format(num))
+    os.system(mainBox+'Rscript ParsSNP_application.r {}.hg19_multianno.txt'.format(num))
+    os.system(mainBox+'mv ParsSNP.output.{}.hg19_multianno.txt /data'.format(num))
+    os.system('mv {}/ParsSNP.output.{}.hg19_multianno.txt {}/{}.hg19_multianno.ParsSNP.output.txt'.format(storePath, num, storePath, num))
+    os.system('sudo docker rm -f {}'.format(mainID))
+    
